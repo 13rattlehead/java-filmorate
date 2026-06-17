9267340a0804042f384bbc1ab200eaf8bf96f192
13rattlehead <rattlehead13@yandex.ru>
2026-06-17 23:35:37 +0300

    исправить тесты

--- src/test/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorageTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,13 +24,77 @@ class UserDbStorageTest {
     @Test
     void testFindUserById() {
 
-        Optional<User> userOptional =
-                userStorage.findById(1L);
+        User user = new User();
+        user.setEmail("test@mail.com");
+        user.setLogin("testlogin");
+        user.setName("Test");
+        user.setBirthday(java.time.LocalDate.of(2000, 1, 1));
 
-        assertThat(userOptional)
-                .isPresent();
+        User created = userStorage.createUser(user);
 
-        assertThat(userOptional.get().getId())
-                .isEqualTo(1L);
+        Optional<User> userOptional = userStorage.findById(created.getId());
+
+        assertThat(userOptional).isPresent();
+        assertThat(userOptional.get().getId()).isEqualTo(created.getId());
     }
+
+    @Test
+    void testCreateAndFindUser() {
+        User user = new User();
+        user.setEmail("a@a.com");
+        user.setLogin("login");
+        user.setName("name");
+        user.setBirthday(LocalDate.of(2000, 1, 1));
+
+        User created = userStorage.createUser(user);
+
+        Optional<User> found = userStorage.findById(created.getId());
+
+        assertThat(found).isPresent();
+        assertThat(found.get().getEmail()).isEqualTo("a@a.com");
+    }
+
+    @Test
+    void testGetUsers() {
+        User user1 = new User();
+        user1.setEmail("a@a.com");
+        user1.setLogin("a");
+        user1.setName("A");
+        user1.setBirthday(LocalDate.of(2000, 1, 1));
+
+        User user2 = new User();
+        user2.setEmail("b@b.com");
+        user2.setLogin("b");
+        user2.setName("B");
+        user2.setBirthday(LocalDate.of(2000, 1, 1));
+
+        userStorage.createUser(user1);
+        userStorage.createUser(user2);
+
+        Collection<User> users = userStorage.getUsers();
+
+        assertThat(users).hasSize(2);
+    }
+
+    @Test
+    void testUpdateUser() {
+        User user = new User();
+        user.setEmail("old@mail.com");
+        user.setLogin("old");
+        user.setName("old");
+        user.setBirthday(LocalDate.of(2000, 1, 1));
+
+        User created = userStorage.createUser(user);
+
+        created.setName("new");
+
+        User updated = userStorage.updateUser(created);
+
+        assertThat(updated.getName()).isEqualTo("new");
+
+        Optional<User> fromDb = userStorage.findById(created.getId());
+        assertThat(fromDb.get().getName()).isEqualTo("new");
+    }
+
+
 }
